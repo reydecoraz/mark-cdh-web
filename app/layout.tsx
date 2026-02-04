e1993ff2d4842fce56166b889b3ce48c05b13035
coorsamexicosistemas <jefaturati@coorsamexico.com>
2026-02-04 11:30:34 -0600

    antes de las modificaiones de fany

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>{children}</body>
     </html>
   )
